5806e89b7e1c0a7aba159d94757bcf2685d56530
pairnon <janpairnon@gmail.com>
2024-07-19 20:59:47 -0700

    Add logging

--- test.js
+++ test.js
@@ -12,22 +12,27 @@ const client = new Client();
 
 client.on("ready", async () => {
   console.log(`${client.user.username} is ready!`);
-  client.user.setStatus('invisible');
+  client.user.setStatus("invisible");
 });
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return false;
   let content = message.content;
   if (!content.includes(userID)) return false;
+  let executorUserID = message.author.id;
+  let executorUsername = message.author.username;
   if (content.includes(prices.command_name)) {
+    console.log(`${executorUserID} (@${executorUsername}) executed a command.`);
     prices.go(message);
     return;
   }
   if (content.includes(ping.command_name)) {
+    console.log(`${executorUserID} (@${executorUsername}) executed a command.`);
     ping.go(message);
     return;
   }
   if (content.includes(showonline.command_name)) {
+    console.log(`${executorUserID} (@${executorUsername}) executed a command.`);
     showonline.go(message, client);
     return;
   }
