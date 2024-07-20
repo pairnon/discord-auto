b8328ee89a3f7a5e2d1905833267e97401a977c3
pairnon <janpairnon@gmail.com>
2024-07-19 23:17:00 -0700

    Refactor

--- commands/dm.js
+++ commands/dm.js
@@ -1,10 +1,19 @@
 export const command_name = "dm";
+const usage = "dm <user id> <message>"
 
 export function go(message, args, client) {
   try {
-    const targetUser = client.users.cache.get(args[0]);
-    targetUser.send(args[1]);
+    const messageToSend = args[1];
+    if (messageToSend == null) {
+      message.reply("### :no_entry_sign: Error: cannot send empty message");
+      return;
+    }
+    const targetID = args[0];
+    const targetUser = client.users.cache.get(targetID);
+    targetUser.send(messageToSend);
+    message.reply(`### :white_check_mark: Sent \`${messageToSend}\` to <@${targetID}>`);
   } catch (error) {
     console.error(`Error: ${error}`);
+    message.reply(`### :bulb: Usage: ${usage}`);
   }
 }
