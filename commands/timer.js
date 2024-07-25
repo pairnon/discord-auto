e650c9d9474d0950fdfe0e389c52967ebc485f44
pairnon <janpairnon@gmail.com>
2024-07-24 20:36:26 -0700

    feat: add minutes for timer

--- commands/timer.js
+++ commands/timer.js
@@ -2,12 +2,51 @@ export const command_name = "timer";
 
 export async function go(message, args, client) {
   const time = args[0];
+  let unit = undefined;
 
-  message.reply(`# :hourglass_flowing_sand: Timer set for ${time} seconds`);
+  if (args[1] != undefined) {
+    unit = args[1];
+  }
 
-  await sleep(time * 1000);
+  if (unit == undefined) {
+    await runSecondsTimer(time, message);
+  } else {
+    switch (unit) {
+      case `s`:
+        await runSecondsTimer(time, message);
+        return;
+      case `m`:
+        if (time == 1) {
+          message.reply(
+            `# :hourglass_flowing_sand: Timer set for ${time} minute`,
+          );
+          await sleep(time * 1000 * 60);
+          message.reply(`# :hourglass: ${time} minute is up!`);
+          return;
+        } else {
+          message.reply(
+            `# :hourglass_flowing_sand: Timer set for ${time} minutes`,
+          );
+          await sleep(time * 1000 * 60);
+          message.reply(`# :hourglass: ${time} minutes is up!`);
+          return;
+        }
+    }
+  }
+}
 
-  message.reply(`# :hourglass: ${time} seconds is up!`);
+async function runSecondsTimer(time, message) {
+  if (time == 1) {
+    message.reply(`# :hourglass_flowing_sand: Timer set for ${time} second`);
+    await sleep(time * 1000);
+    message.reply(`# :hourglass: ${time} second is up!`);
+    return;
+  } else {
+    message.reply(`# :hourglass_flowing_sand: Timer set for ${time} seconds`);
+    await sleep(time * 1000);
+    message.reply(`# :hourglass: ${time} seconds is up!`);
+    return;
+  }
 }
 
 function sleep(ms) {
